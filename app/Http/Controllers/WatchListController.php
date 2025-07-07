eeeae0041e3f264a3d6c943d128acb09c8deff9b
mykill69 <baliviamichael@gmail.com>
2025-07-07 10:09:36 +0800

    update

--- app/Http/Controllers/WatchListController.php
+++ app/Http/Controllers/WatchListController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\TicketDtl;
+use App\Models\Logs;
+use App\Models\watchList;
 
 class WatchListController extends Controller
 {
--- app/Models/watchList.php
+++ app/Models/watchList.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class watchList extends Model
 {
     use HasFactory;
+
+      protected $table = 'watch_list'; 
+    protected $fillable = [
+        'watch_id',
+        
+    ];
+
 }
