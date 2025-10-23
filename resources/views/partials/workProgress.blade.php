2178b9f59e6c51a62a76e8d54f3c358dd0857762
mykill69 <baliviamichael@gmail.com>
2025-10-23 13:32:11 +0800

    update

--- resources/views/partials/workProgress.blade.php
+++ resources/views/partials/workProgress.blade.php
@@ -177,3 +177,9 @@
         </div>
         <!-- /.card-body -->
     </div>
+
+        <script src="template/dist/js/demo.js"></script>
+    <!-- jQuery -->
+    <script src="template/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
